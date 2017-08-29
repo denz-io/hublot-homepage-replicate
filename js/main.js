751443b10bd78429f607c832d6eb5ee75f786472
Denzehl Dacuyan <denzehl.dacuyan@intechsiv.com>
2017-08-29 09:33:48 +0800

    Ticket Title:Updated js Ticket Number:none Description of change

--- js/main.js
+++ js/main.js
@@ -129,3 +129,48 @@ $(document).ready(function(){
 			}
 	});
 });
+
+
+
+//reset scroll property on device size change
+$( window ).resize(function() {
+
+	 if($(window).width() >= 768){
+	//code for scrolling effect when not in mobile
+		$(window).scroll(function(){
+		   var scroll = $(window).scrollTop();
+			  if (scroll > 200) {
+		        $("form").removeClass('show');
+			  	$("#nav-bar").css("height", "70px")
+			    $("#nav-bar").css("background-color" , "#ffffff");
+			  	$(".navbar-nav li > a").css("color" , "#a2a2a2");
+			  	$(".fa-search").css("color" , "#000000");	
+			  	$(".main_logo3").attr('src', 'res/Hublot-logo-219x286.png');
+				$(".main_logo").attr('src', 'res/Hublot_logo.png').addClass('changed'); 
+			  }
+			  else{
+				$("#nav-bar").css("background-color" , 'transparent');
+				$(".navbar-nav li > a").css("color" , "#ffffff");
+			  	$(".fa-search").css("color" , "#ffffff");
+			  	$(".main_logo3").attr('src', 'res/Hublot-logo-white.png');				     
+				$(".main_logo").attr('src', 'res/Hublot_logo_white.png').removeClass('changed');		
+			  }
+	 	})
+
+	}else{
+		$(".navbar-nav li > a").css("color" , "#ffffff");
+		$("#nav-bar").css("height", "40px")
+	//disables search box on scroll
+		$(window).scroll(function(){
+		    var scroll = $(window).scrollTop();
+		   	if ($("form").hasClass('show')) {
+			    if (scroll > 200) {
+			        $("form").removeClass('show');
+				  	$("#nav-bar").css("height", "40px")
+				    $(".main_logo").attr('src', 'res/Hublot_logo.png').addClass('changed');
+			    }
+			}
+	 	})
+	}
+
+});
